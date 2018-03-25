85655033c9785f307b9e16da6ce463df6a791c65
aamin005 <aamin005@gmail>
2018-03-26 02:05:19 +0700

    Integrated new and create action for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,27 @@
 class ProductsController < ApplicationController
+
 	def index
 		@products = Product.all
 	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+    	@product = Product.new(params.require(:product).permit(:title, :subtitle, :price))
+
+    	respond_to do |format|
+      		if @product.save
+		        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+		        format.json { render :show, status: :created, location: @product }
+	      	else
+		        format.html { render :new }
+		        format.json { render json: @product.errors, status: :unprocessable_entity }
+	      end
+	  end
+    end
+
+   
+
 end
